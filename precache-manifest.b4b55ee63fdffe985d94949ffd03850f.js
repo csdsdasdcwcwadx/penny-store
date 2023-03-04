47fcb908479e6ec0e8673d55720fd31ee4c3c013
蔡濡安 <jack0978330@gmail.com>
2023-03-05 00:43:43 +0800

    Updates

---
+++ precache-manifest.b4b55ee63fdffe985d94949ffd03850f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78448432dde76828f7f6",
+    "url": "./app/css/style.css"
+  },
+  {
+    "revision": "78448432dde76828f7f6",
+    "url": "./app/js/index.js"
+  },
+  {
+    "revision": "c93b0b7242e9286b8d1f0b10d0dcd373",
+    "url": "./app/js/index.js.LICENSE.txt"
+  },
+  {
+    "revision": "cddd5e995576fea131bbc74bd9d9fbcf",
+    "url": "./index.html"
+  }
+]);
