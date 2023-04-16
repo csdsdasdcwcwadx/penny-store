11c6ea6400be5550dd633ee51cf3e3e00ca0d416
蔡濡安 <jack0978330@gmail.com>
2023-04-16 15:03:32 +0800

    修正圖片無法存入資料庫問題

--- src/components/Backend/ProductSettings/index.tsx
+++ src/components/Backend/ProductSettings/index.tsx
@@ -16,67 +16,61 @@ function ProductSettings () {
     const { productdetail } = useSelector((store: RootState)=>store);
     const [open_adding, setOpen_adding] = useState(false);
     const [open_fix, setOpen_fix] = useState(false);
-    const [p_img, setP_img] = useState<File>();
     const [fixItem, setFixItem] = useState<I_productinfo>();
 
+    // 新增產品所需要用到的參數
     const p_name = useRef<HTMLInputElement>(null);
     const p_price = useRef<HTMLInputElement>(null);
     const p_amount = useRef<HTMLInputElement>(null);
     const p_type = useRef<HTMLSelectElement>(null);
+    const [p_img, setP_img] = useState<File>();
 
+    // 修改產品所需要用到的參數
     const fix_p_name = useRef<HTMLInputElement>(null);
     const fix_p_price = useRef<HTMLInputElement>(null);
     const fix_p_amount = useRef<HTMLInputElement>(null);
     const fix_p_type = useRef<HTMLSelectElement>(null);
+    const [fix_p_img, setFix_p_img] = useState<File>();
 
     const handle_delete = async (product: I_productinfo) => {
-        try{
-            const { data } = await axios.post<I_productdetail>(`${domain()}/product/removeproduct`, {p_id: product.p_id});
-            alert(data.message);
-            if(data.status) {
-                dispatch(call_getallproduct(''));
-            }
-        }catch(err) {
-            console.error(err);
-        }
-    }
-
-    const handle_add = useCallback(async () =>{
-        if(document.getElementsByClassName('error').length === 0) {
-            const body = {
-                p_name: p_name.current?.value,
-                p_price: p_price.current?.value,
-                p_amount: p_amount.current?.value,
-                p_type: p_type.current?.value,
-            }
-            const formData = new FormData();
-            formData.append('p_name', p_name.current?.value!);
-            formData.append('p_price', p_price.current?.value!);
-            formData.append('p_amount', p_amount.current?.value!);
-            formData.append('p_type', p_type.current?.value!);
-            formData.append('p_img', p_img!);
-            // try {
-            //     const { data } = await axios.post(`${domain()}/product/registryproduct`, formData, {
-            //         headers: {
-            //             'Content-Type': 'multipart/form-data'
-            //         }
-            //     });
-            //     console.log(data)
-    
-            // }catch(err) {
-            //     console.error(err);
-            // }
-            try {
-                const { data } = await axios.post<I_productdetail>(`${domain()}/product/registryproduct`, body);
+        const result = confirm('是否確認要刪除此產品 ? ');
+        if(result) {
+            try{
+                const { data } = await axios.post<I_productdetail>(`${domain()}/product/removeproduct`, {p_id: product.p_id});
+                data.message && alert(data.message);
                 if(data.status) {
-                    setOpen_adding(false);
                     dispatch(call_getallproduct(''));
                 }
             }catch(err) {
                 console.error(err);
             }
         }
-    },[p_img, dispatch])
+    }
+
+    const handle_add = useCallback(async () =>{
+        if(document.getElementsByClassName('error').length === 0) {
+            const formData = new FormData();
+            formData.append('p_name', p_name.current?.value!);
+            formData.append('p_price', p_price.current?.value!);
+            formData.append('p_amount', p_amount.current?.value!);
+            formData.append('p_type', p_type.current?.value!);
+            formData.append('p_img', p_img!);
+            try {
+                const { data } = await axios.post(`${domain()}/product/registryproduct`, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                data.message && alert(data.message);
+                if(data.status) {
+                    location.reload();
+                }
+    
+            }catch(err) {
+                console.error(err);
+            }
+        }
+    },[p_img])
 
     const addingBlock = useCallback(() => {
         return (
@@ -114,29 +108,37 @@ function ProductSettings () {
         )
     },[handle_add, open_adding])
 
-    const handle_fix = useCallback(async () => {
-        if(fixItem) {
-            const { p_id } = fixItem;
-            const body = {
-                p_id: p_id,
-                p_name: fix_p_name.current?.value,
-                p_price: fix_p_price.current?.value,
-                p_amount: fix_p_amount.current?.value,
-                p_type: fix_p_type.current?.value,
-            }
+    const handle_fix = useCallback(async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const button = e.target as HTMLElement;
+        if(button.parentNode?.parentNode?.querySelector('.error') === null) {
+            if(fixItem) {
+                const formData = new FormData();
+                const { p_id } = fixItem;
 
-            try {
-                const { data } = await axios.post(`${domain()}/product/updateproduct`, body);
-                if(data.status) {
-                    setOpen_fix(false);
-                    dispatch(call_getallproduct(''));
+                formData.append('p_id', p_id);
+                formData.append('p_name', fix_p_name.current?.value!);
+                formData.append('p_price', fix_p_price.current?.value!);
+                formData.append('p_amount', fix_p_amount.current?.value!);
+                formData.append('p_type', fix_p_type.current?.value!);
+                formData.append('p_img', fix_p_img!);
+                try {
+                    const { data } = await axios.post(`${domain()}/product/updateproduct`, formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    });
+                    data.message && alert(data.message);
+                    if(data.status) {
+                        setOpen_fix(false);
+                        dispatch(call_getallproduct(''));
+                    }
+        
+                }catch(err) {
+                    console.error(err);
                 }
-    
-            }catch(err) {
-                console.error(err);
-            }   
+            }
         }
-    },[dispatch, fixItem])
+    },[dispatch, fixItem, fix_p_img])
 
     const fixBlock = useCallback(() => {
         if(fixItem) {
@@ -176,19 +178,18 @@ function ProductSettings () {
                                     ref={fix_p_type}>
                                     {
                                         Object.values(E_Page).map((obj, ind) => {
-                                            const defaultOptionValue = p_type;
-                                            return <option key={ind} value={obj} selected={defaultOptionValue === obj}>{handleNavigator(obj as E_Page)}</option>
+                                            return <option key={ind} value={obj}>{handleNavigator(obj as E_Page)}</option>
                                         })
                                     }
                                 </select>
                             </div>
                             <input type="file" onChange={e => {
                                 const file = e.target.files![0];
-                                setP_img(file);
+                                setFix_p_img(file);
                             }}/>
                         </div>
                         <div className={styles.buttongroup}>
-                            <button onClick={handle_fix}>確認</button>
+                            <button onClick={e=>handle_fix(e)}>確認</button>
                         </div>
                     </div>
                 </LightBox>
